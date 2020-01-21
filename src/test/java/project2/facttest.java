4f6a6063bda3c170b7e80a00ec779b22dc4de218
kiran <kiraneng.kiran@gmail.com>
2020-01-21 11:11:34 +0530

    1st commit

---
+++ src/test/java/project2/facttest.java
@@ -0,0 +1,19 @@
+package project2;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class facttest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
